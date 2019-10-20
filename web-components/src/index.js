5b0ff5bb367305ba07ad375cdd9d8a51b3d350b9
kirill <kondratenya21@yahoo.com>
2019-10-20 16:19:00 +0300

    fixed index.js

--- web-components/src/index.js
+++ web-components/src/index.js
@@ -9,7 +9,4 @@ import './components/ChatList';
 import './components/ChatHat';
 import './components/ChatPage';
 
-
-import './images/logo.svg'
-import './images/arrow.png'
 import './images/spongebob.jpg'
