4b88047113780fd56bbfa976a90a880986e33b4d
kirill <kondratenya21@yahoo.com>
2019-11-07 23:18:53 +0300

    to deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './styles/globalStyles.css';
-import * as serviceWorker from './utils/serviceWorker';
-import Messenger from './components/Messenger.js'
 
-render(
-	<Messenger>
-		hello	
-	</Messenger>,
-	document.getElementById('root'),
-);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Messenger from './components/Messenger.js';
+
+render(<Messenger>hello</Messenger>, document.getElementById('root'));
